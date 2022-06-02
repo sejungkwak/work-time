ec7410595e6c76f5a429b73f74f9c6ff4f310fd3
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-02 18:47:53 +0000

    mod: Remove class | menu.py, title.py

--- worktime/app/menu.py
+++ worktime/app/menu.py
@@ -5,33 +5,32 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 
-class Menu:
-    """Creates instances of Menu class"""
+def employee_menu():
+    """Displays the menu for a logged in user as an employee."""
+    numbers = range(1, 8)
+    options = (["Clock In", "Clock Out", "View Clock Card",
+                "View Absence Entitlements", "Book Absence",
+                "Cancel Absence", "Log Out"])
 
-    def employee_menu(self):
-        """Displays the menu for a logged in user as an employee."""
-        numbers = range(1, 8)
-        options = (["Clock In", "Clock Out", "View Clock Card",
-                    "View Absence Entitlements", "Book Absence",
-                    "Cancel Absence", "Log Out"])
+    print("\nPlease choose one of the following options.\n")
+    format_menu(numbers, options)
 
-        print("\nPlease choose one of the following options.\n")
-        self.format_menu(numbers, options)
 
-    def admin_menu(self):
-        """Displays the menu for a logged in user as an admin."""
-        numbers = range(1, 5)
-        options = (["Review Requests", "Review Attendance",
-                    "Update Employee Absence", "Log Out"])
-        print("\nPlease choose one of the following options.\n")
-        self.format_menu(numbers, options)
+def admin_menu():
+    """Displays the menu for a logged in user as an admin."""
+    numbers = range(1, 5)
+    options = (["Review Requests", "Review Attendance",
+                "Update Employee Absence", "Log Out"])
+    print("\nPlease choose one of the following options.\n")
+    format_menu(numbers, options)
 
-    def format_menu(self, numbers, options):
-        """Displays the option number in green font.
 
-        Args:
-            :numbers function: A sequence of numbers.
-            :options list: A list of menu options.
-        """
-        for number, option in zip(numbers, options):
-            print(f"{Fore.GREEN}{number}{Style.RESET_ALL} {option}")
+def format_menu(numbers, options):
+    """Displays the option number in green font.
+
+    Args:
+        :numbers function: A sequence of numbers.
+        :options list: A list of menu options.
+    """
+    for number, option in zip(numbers, options):
+        print(f"{Fore.GREEN}{number}{Style.RESET_ALL} {option}")
--- worktime/app/title.py
+++ worktime/app/title.py
@@ -5,29 +5,27 @@ from art import *
 from worktime.worksheets.employees import Employees
 
 
-class Title:
-    """Creates instances of Title class"""
+def title_main():
+    """Displays the application name at the start of the system."""
+    tprint("Work Time".center(29), font="tarty7")
+    print("\n" + "Time Management System".center(80))
+    print("\n" + "="*80 + "\n")
 
-    def title_main(self):
-        """Displays the application name at the start of the system.
-        """
-        tprint("Work Time".center(29), font="tarty7")
-        print("\n" + "Time Management System".center(80))
-        print("\n" + "="*80 + "\n")
 
-    def title_employee(self, id):
-        """Displays the title and welcome message for the employee portal.
+def title_employee(id):
+    """Displays the title and welcome message for the employee portal.
 
-        Args:
-            :id str: Employee ID that was used to log in.
-        """
-        fname = Employees(id).get_fname()
-        tprint("Employee Portal".center(18), font="rectangles")
-        print(f"Welcome back, {fname}!".center(80))
-        print("\n" + "="*80)
+    Args:
+        :id str: Employee ID that was used to log in.
+    """
+    fname = Employees(id).get_fname()
+    tprint("Employee Portal".center(18), font="rectangles")
+    print(f"Welcome back, {fname}!".center(80))
+    print("\n" + "="*80)
 
-    def title_admin(self):
-        """Displays the title and welcome message for the admin portal."""
-        tprint("Admin Portal".center(24), font="rectangles")
-        print("Welcome back!".center(80))
-        print("\n" + "="*80)
+
+def title_admin():
+    """Displays the title and welcome message for the admin portal."""
+    tprint("Admin Portal".center(24), font="rectangles")
+    print("Welcome back!".center(80))
+    print("\n" + "="*80)
