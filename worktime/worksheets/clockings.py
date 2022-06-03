c45b3b9309cff77f1a6aafb0a0af3da8589a486b
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-03 14:23:43 +0000

    refactor: Use function to convert date format | clockings.py

--- worktime/worksheets/clockings.py
+++ worktime/worksheets/clockings.py
@@ -10,23 +10,14 @@ class Clockings:
         :id str: An employee ID
     """
 
+    today = utility.get_current_datetime()["date"]
+
     def __init__(self, id=None):
         self.id = id
         self.worksheet = auth.SHEET.worksheet("clockings")
         self.clockings = self.worksheet.get_all_values()
         self.clock_in_col = "C"
         self.clock_out_col = "D"
-        self.today = utility.get_datetime()["date"]
-
-    def get_one_clocking(self, target_date=None):
-        """Return clocking data with row number in a dictionary."""
-        if target_date is None:
-            target_date = self.today
-        for row, clocking in enumerate(self.clockings, start=1):
-            id, date, clock_in, clock_out = clocking
-            if id == self.id and date == target_date:
-                return ({"row": row, "id": id, "date": date,
-                        "start_time": clock_in, "out_time": clock_out})
 
     def add_clocking(self, data):
         """Add clocking data to the worksheet."""
@@ -34,23 +25,31 @@ class Clockings:
 
     def update_clock_in(self, time):
         """Replace an existing clock in data with a new one."""
-        row = self.get_today_clocking()["row"]
+        row = self.get_one_clocking()["row"]
         self.worksheet.update(f"{self.clock_in_col}{row}", time)
 
     def update_clock_out(self, time):
         """Replace an existing clock out data with a new one."""
-        row = self.get_today_clocking()["row"]
+        row = self.get_one_clocking()["row"]
         self.worksheet.update(f"{self.clock_out_col}{row}", time)
+
+    def get_one_clocking(self, target_date=None):
+        """Return clocking data with row number in a dictionary."""
+        if target_date is None:
+            target_date = self.today
+        for row, clocking in enumerate(self.clockings, start=1):
+            id, date, clock_in, clock_out = clocking
+            date = utility.convert_date(date)
+            if id == self.id and date == target_date:
+                return ({"row": row, "id": id, "date": target_date,
+                        "start_time": clock_in, "end_time": clock_out})
 
     def get_week_clockings(self, date=None):
         """Return a week's clocking data in a list of lists"""
         if date is None:
             date = self.today
-        datelist = date.split("/")
-        year = int(datelist[2])
-        month = int(datelist[1])
-        day = int(datelist[0])
-        weekdays = utility.get_week(year, month, day)
+        target_date = utility.convert_date(date)
+        weekdays = utility.get_week(target_date)
         result = []
         for weekday in weekdays:
             if self.get_one_clocking(weekday) is not None:
