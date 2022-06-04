0a1755cbf84b13a22689190323c065a5ff67bb0e
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-04 19:29:29 +0000

    feat: Add code for reveiw request option | admin.py

--- worktime/app/admin.py
+++ worktime/app/admin.py
@@ -2,8 +2,8 @@
 from colorama import init, Fore, Style
 
 # Custom Package
-from worktime.app import menu, title, validations
-from worktime.worksheets import requests
+from worktime.app import menu, tables, title, validations
+from worktime.worksheets import employees, requests
 
 # colorama method to enable it on Windows
 init(autoreset=True)
@@ -14,12 +14,7 @@ def admin_main():
     Run a while loop until the user inputs a valid number.
     """
     title.title_admin()
-    new_requests = requests.Requests().get_new_requests()
-    if len(new_requests) > 0:
-        print(f"\n{Fore.GREEN}You have",
-              f"{len(new_requests)} request(s) to review.")
-    else:
-        print("\nThere is no more requests to review.")
+    requests_notification_message()
     while True:
         menu.admin_menu()
         choice = input("\nPlease enter a number to continue:\n").strip()
@@ -27,10 +22,35 @@ def admin_main():
             break
 
     if choice == "1":
-        pass
+        review_requests()
     elif choice == "2":
         pass
     elif choice == "3":
         pass
     else:
         pass
+
+
+def requests_notification_message():
+    """Check if there are new requests and display the result."""
+    new_requests = requests.Requests().get_new_requests()
+    if len(new_requests) > 0:
+        print(f"\n{Fore.GREEN}You have",
+              f"{Fore.GREEN}{len(new_requests)} request(s) to review.")
+    else:
+        print("\nThere is no more requests to review.")
+
+
+def review_requests():
+    """Display new requests and ask the user to choose a number."""
+    requests_notification_message()
+    new_requests = requests.Requests().get_new_requests()
+    if len(new_requests) > 0:
+        print("Getting data...")
+        tables.display_new_requests()
+        while True:
+            id_list = [int(list[0]) for list in new_requests]
+            print("Enter the ID you want to approve or reject.")
+            choice = input("Please enter your answer here:\n").strip()
+            if validations.validate_choice_number(choice, id_list):
+                break
