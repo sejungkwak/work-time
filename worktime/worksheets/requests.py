d99d9e39c1c509477b878ea407b8a0ad18f4d3e0
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-04 17:37:07 +0000

    feat: Add code to pull new requests data | requests.py

--- worktime/worksheets/requests.py
+++ worktime/worksheets/requests.py
@@ -98,3 +98,13 @@ class Requests:
                     not request[-2] == "False" and request[-1] == "False"):
                 cancellable.append(request)
         return cancellable
+
+    def get_new_requests(self):
+        new_requests = []
+        today = utility.convert_date(self.today)
+        for request in self.requests:
+            date = utility.convert_date(request[2])
+            if ((date - today).days > 0 and
+                    request[-2] == "/" and request[-1] == "False"):
+                new_requests.append(request)
+        return new_requests
