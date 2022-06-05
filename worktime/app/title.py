50e6c316c70ce4878d271a5876863e85e897f11c
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-05 01:23:16 +0000

    feat: Add system end message | title.py

--- worktime/app/title.py
+++ worktime/app/title.py
@@ -2,22 +2,25 @@
 from art import *
 
 # Custom Package
+from worktime.app import utility
 from worktime.worksheets.employees import Employees
 
 
 def title_main():
-    """Displays the application name at the start of the system."""
+    """Display the application name at the start of the system."""
+    utility.clear()
     tprint("Work Time".center(29), font="tarty7")
     print("\n" + "Time Management System".center(80))
     print("\n" + "="*80 + "\n")
 
 
 def title_employee(id):
-    """Displays the title and welcome message for the employee portal.
+    """Display the title and welcome message for the employee portal.
 
     Args:
         :id str: Employee ID that was used to log in.
     """
+    utility.clear()
     fname = Employees(id).get_fname()
     tprint("Employee Portal".center(18), font="rectangles")
     print(f"Welcome back, {fname}!".center(80))
@@ -25,7 +28,15 @@ def title_employee(id):
 
 
 def title_admin():
-    """Displays the title and welcome message for the admin portal."""
+    """Display the title and welcome message for the admin portal."""
+    utility.clear()
     tprint("Admin Portal".center(24), font="rectangles")
     print("Welcome back!".center(80))
     print("\n" + "="*80)
+
+
+def title_end():
+    """Display an end of the system title."""
+    utility.clear()
+    print("\n"*5)
+    tprint("Thank You!".center(25), font="rectangles")
