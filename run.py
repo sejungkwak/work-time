99d157bfe7d7cd82c84ca8d98831290e366d6ff7
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-11 19:57:59 +0000

    feat: Add app information for help command | run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import sys
 import stdiomask
 
 # Custom Packages
-from worktime.app import admin, employee, title, utility, validations
+from worktime.app import admin, employee, tables, title, utility, validations
 from worktime.worksheets import credentials
 
 
@@ -14,17 +14,20 @@ def get_employee_id():
     Run a while loop until the user types "help" or a valid ID.
     """
     ids = credentials.Credentials().ids()
+    infor_printed = False
+
     while True:
         print(f"Please enter {utility.cyan('Employee ID.')}")
-        print("To contact the system administrator, enter",
-              f"{utility.cyan('help')} instead.")
+        if not infor_printed:
+            print("For more information about Work Time,",
+                  f"type {utility.cyan('help')} and press enter.")
         entered_id = input(f"{utility.cyan('>>>')}\n").upper().strip()
         utility.clear()
 
         if entered_id == "HELP":
-            break
-
-        if validations.validate_id(entered_id, ids):
+            help_()
+            infor_printed = True
+        elif validations.validate_id(entered_id, ids):
             get_pw(entered_id)
             break
 
@@ -39,16 +42,10 @@ def get_pw(id):
     pw = credentials.Credentials().pw(id)
     while True:
         print(f"Please enter {utility.cyan('Password')}.")
-        print("To contact the system administrator, enter",
-              f"{utility.cyan('help')} instead.")
         password = stdiomask.getpass(prompt=f"{utility.cyan('>>>')}\n")
         utility.clear()
 
-        if password.upper() == "HELP":
-            break
-
-        is_valid = validations.validate_pw(password, pw)
-        if is_valid:
+        if validations.validate_pw(password, pw):
             if id == "ADMIN":
                 title.title_admin()
                 admin.admin_main()
@@ -57,6 +54,28 @@ def get_pw(id):
                 title.title_employee(id)
                 employee.employee_main(id)
                 break
+
+
+def help_():
+    """Print application information."""
+    text = f"""
+{utility.cyan('About the application')}
+Work Time is an employee time management system. It provides an employee
+clocking system, attendance tracking and absence management.
+
+{utility.cyan('Employee Portal')}
+It offers the following 6 options to choose from: Clock In, Clock Out, View
+Clock Card, View Absence Entitlements, Book Absence, Cancel Absence.
+
+{utility.cyan('Admin Portal')}
+It offers the following 4 options to choose from: Review Requests, Review
+Attendance, Add absence, Update Clock Card.
+
+{utility.cyan('Contact the developer')}
+If you would like to report a bug, suggest an idea or require additional help,
+please email me at kwak.sejung@gmail.com
+"""
+    tables.display_table([[text]])
 
 
 if __name__ == "__main__":
